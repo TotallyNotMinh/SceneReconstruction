c53828da07911cbaed3a48d3bd16a82d13171333
Lznkz <dragonmine012@gmail.com>
2026-08-06 00:13:06 +0700

    remove old file

--- PartC/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd SceneReconstruction/PartC
-git clone https://github.com/TencentARC/InstantMesh.git
-cd InstantMesh
-pip install requirement.txt
-pip uninstall -y flax   #this can cause problem in InstantMesh
